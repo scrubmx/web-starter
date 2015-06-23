e672fc9f0e22f6beedf4d7cc973c4c164ae8d9ce
scrub <scrub.mx@gmail.com>
2015-06-23 00:32:27 -0400

    added better error reporting in the browserify task with gulp-util

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
+import gutil from 'gulp-util';
 import babelify from 'babelify';
 import jshint from 'gulp-jshint';
 import browserify from 'browserify';
@@ -9,35 +10,45 @@ import source from 'vinyl-source-stream';
 const SCRIPTS_DIR = 'resources/assets/scripts';
 const STYLES_DIR = 'resources/assets/sass';
 
+/**
+ * Handle error reporting
+ * @param  object error [description]
+ */
+function handleError(error){
+  gutil.log(gutil.colors.red('Error: ' + error.message));
+  this.emit('end');
+}
+
 // Lint javascript files
 gulp.task('lint', () => {
   return gulp.src(SCRIPTS_DIR+'/**/*.js')
-    .pipe(jshint({"esnext": true}))
+    .pipe(jshint({'esnext': true}))
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
 });
 
+// Use ES6 modules in the browser
 gulp.task('browserify', () => {
   return browserify(SCRIPTS_DIR+'/main.js')
     .transform(babelify, { stage: 0 })
     .bundle()
-    .on('error', function(e){
-        console.log(e.message);
-        this.emit('end');
-    })
+    .on('error', handleError)
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./public/js'));
 });
 
+// Compile SASS to regular CSS
 gulp.task('sass', () => {
-  gulp.src(STYLES_DIR+'/style.scss')
+  return gulp.src(STYLES_DIR+'/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
 
+// Recompile files when saved
 gulp.task('watch', ['lint', 'browserify', 'sass'], () => {
-    gulp.watch(SCRIPTS_DIR+'/**/*.js', ['lint', 'browserify']);
-    gulp.watch(STYLES_DIR+'/**/*.scss', ['sass']);
+  gulp.watch(SCRIPTS_DIR+'/**/*.js', ['lint', 'browserify']);
+  gulp.watch(STYLES_DIR+'/**/*.scss', ['sass']);
 });
 
+// Default gulp task
 gulp.task('default', ['watch']);
